cf0f487dcd419805d325a7a7396094f393164f75
sethupathy06 <vishnunivas006@gmail.com>
2026-02-13 22:30:46 +0530

    containsDuplicate

---
+++ Easy/containsDuplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       HashSet<Integer> h = new HashSet<>();
+       for(int n:nums){
+         if(h.contains(n))
+          return true;
+         h.add(n);
+       }
+       return false;
+   }
+}
